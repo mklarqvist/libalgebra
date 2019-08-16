eb126b4a0f46a0debff6b136286d18483c7d6fc3
Marcus D. R. Klarqvist <mk819@cam.ac.uk>
2019-08-16 10:38:50 +0100

    naive

--- benchmark.cpp
+++ benchmark.cpp
@@ -4,6 +4,93 @@
 #include <random>
 #include <chrono>
 
+uint8_t lookup8bit[256] = {
+	/* 0 */ 0, /* 1 */ 1, /* 2 */ 1, /* 3 */ 2,
+	/* 4 */ 1, /* 5 */ 2, /* 6 */ 2, /* 7 */ 3,
+	/* 8 */ 1, /* 9 */ 2, /* a */ 2, /* b */ 3,
+	/* c */ 2, /* d */ 3, /* e */ 3, /* f */ 4,
+	/* 10 */ 1, /* 11 */ 2, /* 12 */ 2, /* 13 */ 3,
+	/* 14 */ 2, /* 15 */ 3, /* 16 */ 3, /* 17 */ 4,
+	/* 18 */ 2, /* 19 */ 3, /* 1a */ 3, /* 1b */ 4,
+	/* 1c */ 3, /* 1d */ 4, /* 1e */ 4, /* 1f */ 5,
+	/* 20 */ 1, /* 21 */ 2, /* 22 */ 2, /* 23 */ 3,
+	/* 24 */ 2, /* 25 */ 3, /* 26 */ 3, /* 27 */ 4,
+	/* 28 */ 2, /* 29 */ 3, /* 2a */ 3, /* 2b */ 4,
+	/* 2c */ 3, /* 2d */ 4, /* 2e */ 4, /* 2f */ 5,
+	/* 30 */ 2, /* 31 */ 3, /* 32 */ 3, /* 33 */ 4,
+	/* 34 */ 3, /* 35 */ 4, /* 36 */ 4, /* 37 */ 5,
+	/* 38 */ 3, /* 39 */ 4, /* 3a */ 4, /* 3b */ 5,
+	/* 3c */ 4, /* 3d */ 5, /* 3e */ 5, /* 3f */ 6,
+	/* 40 */ 1, /* 41 */ 2, /* 42 */ 2, /* 43 */ 3,
+	/* 44 */ 2, /* 45 */ 3, /* 46 */ 3, /* 47 */ 4,
+	/* 48 */ 2, /* 49 */ 3, /* 4a */ 3, /* 4b */ 4,
+	/* 4c */ 3, /* 4d */ 4, /* 4e */ 4, /* 4f */ 5,
+	/* 50 */ 2, /* 51 */ 3, /* 52 */ 3, /* 53 */ 4,
+	/* 54 */ 3, /* 55 */ 4, /* 56 */ 4, /* 57 */ 5,
+	/* 58 */ 3, /* 59 */ 4, /* 5a */ 4, /* 5b */ 5,
+	/* 5c */ 4, /* 5d */ 5, /* 5e */ 5, /* 5f */ 6,
+	/* 60 */ 2, /* 61 */ 3, /* 62 */ 3, /* 63 */ 4,
+	/* 64 */ 3, /* 65 */ 4, /* 66 */ 4, /* 67 */ 5,
+	/* 68 */ 3, /* 69 */ 4, /* 6a */ 4, /* 6b */ 5,
+	/* 6c */ 4, /* 6d */ 5, /* 6e */ 5, /* 6f */ 6,
+	/* 70 */ 3, /* 71 */ 4, /* 72 */ 4, /* 73 */ 5,
+	/* 74 */ 4, /* 75 */ 5, /* 76 */ 5, /* 77 */ 6,
+	/* 78 */ 4, /* 79 */ 5, /* 7a */ 5, /* 7b */ 6,
+	/* 7c */ 5, /* 7d */ 6, /* 7e */ 6, /* 7f */ 7,
+	/* 80 */ 1, /* 81 */ 2, /* 82 */ 2, /* 83 */ 3,
+	/* 84 */ 2, /* 85 */ 3, /* 86 */ 3, /* 87 */ 4,
+	/* 88 */ 2, /* 89 */ 3, /* 8a */ 3, /* 8b */ 4,
+	/* 8c */ 3, /* 8d */ 4, /* 8e */ 4, /* 8f */ 5,
+	/* 90 */ 2, /* 91 */ 3, /* 92 */ 3, /* 93 */ 4,
+	/* 94 */ 3, /* 95 */ 4, /* 96 */ 4, /* 97 */ 5,
+	/* 98 */ 3, /* 99 */ 4, /* 9a */ 4, /* 9b */ 5,
+	/* 9c */ 4, /* 9d */ 5, /* 9e */ 5, /* 9f */ 6,
+	/* a0 */ 2, /* a1 */ 3, /* a2 */ 3, /* a3 */ 4,
+	/* a4 */ 3, /* a5 */ 4, /* a6 */ 4, /* a7 */ 5,
+	/* a8 */ 3, /* a9 */ 4, /* aa */ 4, /* ab */ 5,
+	/* ac */ 4, /* ad */ 5, /* ae */ 5, /* af */ 6,
+	/* b0 */ 3, /* b1 */ 4, /* b2 */ 4, /* b3 */ 5,
+	/* b4 */ 4, /* b5 */ 5, /* b6 */ 5, /* b7 */ 6,
+	/* b8 */ 4, /* b9 */ 5, /* ba */ 5, /* bb */ 6,
+	/* bc */ 5, /* bd */ 6, /* be */ 6, /* bf */ 7,
+	/* c0 */ 2, /* c1 */ 3, /* c2 */ 3, /* c3 */ 4,
+	/* c4 */ 3, /* c5 */ 4, /* c6 */ 4, /* c7 */ 5,
+	/* c8 */ 3, /* c9 */ 4, /* ca */ 4, /* cb */ 5,
+	/* cc */ 4, /* cd */ 5, /* ce */ 5, /* cf */ 6,
+	/* d0 */ 3, /* d1 */ 4, /* d2 */ 4, /* d3 */ 5,
+	/* d4 */ 4, /* d5 */ 5, /* d6 */ 5, /* d7 */ 6,
+	/* d8 */ 4, /* d9 */ 5, /* da */ 5, /* db */ 6,
+	/* dc */ 5, /* dd */ 6, /* de */ 6, /* df */ 7,
+	/* e0 */ 3, /* e1 */ 4, /* e2 */ 4, /* e3 */ 5,
+	/* e4 */ 4, /* e5 */ 5, /* e6 */ 5, /* e7 */ 6,
+	/* e8 */ 4, /* e9 */ 5, /* ea */ 5, /* eb */ 6,
+	/* ec */ 5, /* ed */ 6, /* ee */ 6, /* ef */ 7,
+	/* f0 */ 4, /* f1 */ 5, /* f2 */ 5, /* f3 */ 6,
+	/* f4 */ 5, /* f5 */ 6, /* f6 */ 6, /* f7 */ 7,
+	/* f8 */ 5, /* f9 */ 6, /* fa */ 6, /* fb */ 7,
+	/* fc */ 6, /* fd */ 7, /* fe */ 7, /* ff */ 8
+};
+
+
+std::uint64_t popcnt_lookup_8bit(const uint8_t* data, const size_t n) {
+
+    size_t result = 0;
+
+    size_t i = 0;
+    while (i + 4 <= n) {
+        result += lookup8bit[data[i]]; i++;
+        result += lookup8bit[data[i]]; i++;
+        result += lookup8bit[data[i]]; i++;
+        result += lookup8bit[data[i]]; i++;
+    }
+
+    while (i < n) {
+        result += lookup8bit[data[i]]; i++;
+    }
+
+    return result;
+}
+
 struct bench_unit {
     bench_unit() : valid(false), cycles(0), cycles_local(0), times(0), times_local(0){}
 
@@ -39,6 +126,87 @@ void generate_random_data(uint64_t* data, uint32_t range, uint32_t n) {
         n_unique += (data[val / 64] & (1ULL << (val % 64))) == 0;
         data[val / 64] |= 1ULL << (val % 64);
     }
+}
+
+#if !defined(__clang__) && !defined(_MSC_VER)
+__attribute__((optimize("no-tree-vectorize")))
+#endif
+uint64_t intersect_scalar_naive_nosimd(const uint64_t* data1,const uint64_t* data2, uint32_t len) {
+    uint64_t total = 0;
+    // for (int i = 0; i < len; ++i) {
+    //     total += STORM_popcount64(data1[i] & data2[i]);
+    // }
+
+    uint64_t diff;
+    uint8_t* b8 = (uint8_t*)&diff;
+    for (int i = 0; i < len; ++i) {
+        // total += STORM_popcount64(data1[i] & data2[i]);
+        diff = data1[i] & data2[i];
+        total += lookup8bit[b8[0]];
+        total += lookup8bit[b8[1]];
+        total += lookup8bit[b8[2]];
+        total += lookup8bit[b8[3]];
+        total += lookup8bit[b8[4]];
+        total += lookup8bit[b8[5]];
+        total += lookup8bit[b8[6]];
+        total += lookup8bit[b8[7]];
+    }
+
+    return total;
+}
+
+#if !defined(__clang__) && !defined(_MSC_VER)
+__attribute__((optimize("no-tree-vectorize")))
+#endif
+uint64_t union_scalar_naive_nosimd(const uint64_t* data1,const uint64_t* data2, uint32_t len) {
+    uint64_t total = 0;
+    // for (int i = 0; i < len; ++i) {
+    //     total += STORM_popcount64(data1[i] | data2[i]);
+    // }
+
+    uint64_t diff;
+    uint8_t* b8 = (uint8_t*)&diff;
+    for (int i = 0; i < len; ++i) {
+        // total += STORM_popcount64(data1[i] | data2[i]);
+        diff = data1[i] | data2[i];
+        total += lookup8bit[b8[0]];
+        total += lookup8bit[b8[1]];
+        total += lookup8bit[b8[2]];
+        total += lookup8bit[b8[3]];
+        total += lookup8bit[b8[4]];
+        total += lookup8bit[b8[5]];
+        total += lookup8bit[b8[6]];
+        total += lookup8bit[b8[7]];
+    }
+
+    return total;
+}
+
+#if !defined(__clang__) && !defined(_MSC_VER)
+__attribute__((optimize("no-tree-vectorize")))
+#endif
+uint64_t diff_scalar_naive_nosimd(const uint64_t* data1,const uint64_t* data2, uint32_t len) {
+    uint64_t total = 0;
+    // for (int i = 0; i < len; ++i) {
+    //     total += STORM_popcount64(data1[i] ^ data2[i]);
+    // }
+
+    uint64_t diff;
+    uint8_t* b8 = (uint8_t*)&diff;
+    for (int i = 0; i < len; ++i) {
+        // total += STORM_popcount64(data1[i] ^ data2[i]);
+        diff = data1[i] ^ data2[i];
+        total += lookup8bit[b8[0]];
+        total += lookup8bit[b8[1]];
+        total += lookup8bit[b8[2]];
+        total += lookup8bit[b8[3]];
+        total += lookup8bit[b8[4]];
+        total += lookup8bit[b8[5]];
+        total += lookup8bit[b8[6]];
+        total += lookup8bit[b8[7]];
+    }
+
+    return total;
 }
 
 // Definition for microsecond timer.
@@ -82,6 +250,7 @@ asm   volatile("RDTSCP\n\t"
                "CPUID\n\t": "=r" (cycles_high1), "=r" (cycles_low1):: "%rax", "%rbx", "%rcx", "%rdx");
 #endif
 
+    uint64_t total = 0;
     for (int i = 0; i < iterations; ++i) {
         generate_random_data(data1, range, n_values);
         generate_random_data(data2, range, n_values);
@@ -102,7 +271,7 @@ asm   volatile("RDTSCP\n\t"
                     "mov %%eax, %1\n\t": "=r" (cycles_high), "=r" (cycles_low):: "%rax", "%rbx", "%rcx", "%rdx");
 #endif
     // Call argument subroutine pointer.
-    uint64_t ret = (*f)(data1, data2, n_bitmaps);
+    total += (*f)(data1, data2, n_bitmaps);
 
 #ifndef _MSC_VER 
     asm   volatile("RDTSCP\n\t"
@@ -148,7 +317,7 @@ asm   volatile("RDTSCP\n\t"
     variance /= clocks.size();
     stdDeviation = sqrt(variance);
 
-    std::cout << name << "\t" << n_bitmaps << "\t" << 
+    std::cout << name << "\t" << n_bitmaps << "\t" << total << "\t" << 
         mean_cycles << "\t" <<
         min_c << "(" << min_c/mean_cycles << ")" << "\t" << 
         max_c << "(" << max_c/mean_cycles << ")" << "\t" <<
@@ -216,16 +385,19 @@ int benchmark(uint32_t range, uint32_t n_values) {
     {
         // Align some bitmaps.
         uint32_t n_bitmaps = ceil(range / 64.0);
-        uint64_t* bitmaps  = (uint64_t*)STORM_aligned_malloc(STORM_get_alignment(), 262144*sizeof(uint64_t));
-        uint64_t* bitmaps2 = (uint64_t*)STORM_aligned_malloc(STORM_get_alignment(), 262144*sizeof(uint64_t));
+        uint64_t* bitmaps  = (uint64_t*)STORM_aligned_malloc(STORM_get_alignment(), 1048576*sizeof(uint64_t));
+        uint64_t* bitmaps2 = (uint64_t*)STORM_aligned_malloc(STORM_get_alignment(), 1048576*sizeof(uint64_t));
 
-        std::vector<uint32_t> ranges = {128,256,512,1024,2048,4096,8192,65536,262144};
+        std::vector<uint32_t> ranges = {128,256,512,1024,2048,4096,8192,65536,262144,1048576};
 
         for (int i = 0; i < ranges.size(); ++i) {
             bench_unit unit_intsec, unit_union, unit_diff;
-            set_algebra_wrapper("intersect",STORM_get_intersect_count_func(n_bitmaps), 1000, bitmaps, bitmaps2, range, n_values, ranges[i], unit_intsec);
-            set_algebra_wrapper("union",STORM_get_union_count_func(n_bitmaps), 1000, bitmaps, bitmaps2, range, n_values, ranges[i], unit_union);
-            set_algebra_wrapper("diff",STORM_get_diff_count_func(n_bitmaps), 1000, bitmaps, bitmaps2, range, n_values, ranges[i], unit_diff);
+            set_algebra_wrapper("intersect-naive",&intersect_scalar_naive_nosimd, 2000, bitmaps, bitmaps2, range, n_values, ranges[i], unit_intsec);
+            set_algebra_wrapper("intersect",STORM_get_intersect_count_func(n_bitmaps), 2000, bitmaps, bitmaps2, range, n_values, ranges[i], unit_intsec);
+            set_algebra_wrapper("union-naive",&union_scalar_naive_nosimd, 2000, bitmaps, bitmaps2, range, n_values, ranges[i], unit_intsec);
+            set_algebra_wrapper("union",STORM_get_union_count_func(n_bitmaps), 2000, bitmaps, bitmaps2, range, n_values, ranges[i], unit_union);
+            set_algebra_wrapper("diff-naive",&diff_scalar_naive_nosimd, 2000, bitmaps, bitmaps2, range, n_values, ranges[i], unit_intsec);
+            set_algebra_wrapper("diff",STORM_get_diff_count_func(n_bitmaps), 2000, bitmaps, bitmaps2, range, n_values, ranges[i], unit_diff);
         }
 
         // Clean up.
