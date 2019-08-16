87608a44a59188536cd777b95acf206eeeff197d
Marcus D. R. Klarqvist <mk819@cam.ac.uk>
2019-08-16 23:32:11 +0100

    linux benchmark

--- benchmark.cpp
+++ benchmark.cpp
@@ -6,6 +6,268 @@
 #include <string>
 #include <vector>
 
+void generate_random_data(uint64_t* data, uint32_t range, uint32_t n) {
+    // PRNG
+    std::uniform_int_distribution<uint32_t> distr(0, range-1); // right inclusive
+    std::random_device rd;  // obtain a random number from hardware
+    std::mt19937 eng(rd()); // seed the generator
+
+     // Generate some random data.
+    uint32_t n_unique = 0;
+    // while (n_unique < n) {
+    for (int i = 0; i < n; ++i) {
+        uint32_t val = distr(eng);
+        n_unique += (data[val / 64] & (1ULL << (val % 64))) == 0;
+        data[val / 64] |= 1ULL << (val % 64);
+    }
+}
+
+#ifdef __linux__
+
+#include <asm/unistd.h>       // for __NR_perf_event_open
+#include <linux/perf_event.h> // for perf event constants
+#include <sys/ioctl.h>        // for ioctl
+#include <unistd.h>           // for syscall
+#include <iostream>
+#include <cerrno>  // for errno
+#include <cstring> // for memset
+#include <stdexcept>
+
+#include <vector>
+
+template <int TYPE = PERF_TYPE_HARDWARE> 
+class LinuxEvents {
+    int fd;
+    bool working;
+    perf_event_attr attribs;
+    int num_events;
+    std::vector<uint64_t> temp_result_vec;
+    std::vector<uint64_t> ids;
+
+public:
+    explicit LinuxEvents(std::vector<int> config_vec) : fd(0), working(true) {
+        memset(&attribs, 0, sizeof(attribs));
+        attribs.type = TYPE;
+        attribs.size = sizeof(attribs);
+        attribs.disabled = 1;
+        attribs.exclude_kernel = 1;
+        attribs.exclude_hv = 1;
+
+        attribs.sample_period = 0;
+        attribs.read_format = PERF_FORMAT_GROUP | PERF_FORMAT_ID;
+        const int pid = 0;  // the current process
+        const int cpu = -1; // all CPUs
+        const unsigned long flags = 0;
+
+        int group = -1; // no group
+        num_events = config_vec.size();
+        uint32_t i = 0;
+        for (auto config : config_vec) {
+            attribs.config = config;
+            fd = syscall(__NR_perf_event_open, &attribs, pid, cpu, group, flags);
+            if (fd == -1) {
+                report_error("perf_event_open");
+            }
+                ioctl(fd, PERF_EVENT_IOC_ID, &ids[i++]);
+                if (group == -1) {
+                group = fd;
+            }
+        }
+
+        temp_result_vec.resize(num_events * 2 + 1);
+    }
+
+    ~LinuxEvents() { close(fd); }
+
+    inline void start() {
+        if (ioctl(fd, PERF_EVENT_IOC_RESET, PERF_IOC_FLAG_GROUP) == -1) {
+            report_error("ioctl(PERF_EVENT_IOC_RESET)");
+        }
+
+        if (ioctl(fd, PERF_EVENT_IOC_ENABLE, PERF_IOC_FLAG_GROUP) == -1) {
+            report_error("ioctl(PERF_EVENT_IOC_ENABLE)");
+        }
+    }
+
+    inline void end(std::vector<unsigned long long> &results) {
+        if (ioctl(fd, PERF_EVENT_IOC_DISABLE, PERF_IOC_FLAG_GROUP) == -1) {
+            report_error("ioctl(PERF_EVENT_IOC_DISABLE)");
+        }
+
+        if (read(fd, &temp_result_vec[0], temp_result_vec.size() * 8) == -1) {
+            report_error("read");
+        }
+        // our actual results are in slots 1,3,5, ... of this structure
+        // we really should be checking our ids obtained earlier to be safe
+        for (uint32_t i = 1; i < temp_result_vec.size(); i += 2) {
+            results[i / 2] = temp_result_vec[i];
+        }
+    }
+
+private:
+    void report_error(const std::string &context) {
+    if (working)
+        std::cerr << (context + ": " + std::string(strerror(errno))) << std::endl;
+        working = false;
+    }
+};
+
+std::vector<unsigned long long>
+compute_mins(std::vector< std::vector<unsigned long long> > allresults) {
+    if (allresults.size() == 0)
+        return std::vector<unsigned long long>();
+    
+    std::vector<unsigned long long> answer = allresults[0];
+    
+    for (size_t k = 1; k < allresults.size(); k++) {
+        assert(allresults[k].size() == answer.size());
+        for (size_t z = 0; z < answer.size(); z++) {
+            if (allresults[k][z] < answer[z])
+                answer[z] = allresults[k][z];
+        }
+    }
+    return answer;
+}
+
+std::vector<double>
+compute_averages(std::vector< std::vector<unsigned long long> > allresults) {
+    if (allresults.size() == 0)
+        return std::vector<double>();
+    
+    std::vector<double> answer(allresults[0].size());
+    
+    for (size_t k = 0; k < allresults.size(); k++) {
+        assert(allresults[k].size() == answer.size());
+        for (size_t z = 0; z < answer.size(); z++) {
+            answer[z] += allresults[k][z];
+        }
+    }
+
+    for (size_t z = 0; z < answer.size(); z++) {
+        answer[z] /= allresults.size();
+    }
+    return answer;
+}
+
+int linux_set_algebra_wrapper(std::string name,
+    STORM_compute_func f, 
+    int iterations,
+    uint64_t* STORM_RESTRICT data1, 
+    uint64_t* STORM_RESTRICT data2, 
+    uint32_t range,
+    uint32_t n_values,
+    uint32_t n_bitmaps, 
+    bool verbose) 
+{
+    std::vector<int> evts;
+    evts.push_back(PERF_COUNT_HW_CPU_CYCLES);
+    evts.push_back(PERF_COUNT_HW_INSTRUCTIONS);
+    evts.push_back(PERF_COUNT_HW_BRANCH_MISSES);
+    evts.push_back(PERF_COUNT_HW_CACHE_REFERENCES);
+    evts.push_back(PERF_COUNT_HW_CACHE_MISSES);
+    evts.push_back(PERF_COUNT_HW_REF_CPU_CYCLES);
+    LinuxEvents<PERF_TYPE_HARDWARE> unified(evts);
+    std::vector<unsigned long long> results; // tmp buffer
+    std::vector< std::vector<unsigned long long> > allresults;
+    results.resize(evts.size());
+    
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> dis(0, 0xFFFF);
+
+    volatile uint64_t total = 0; // voltatile to prevent compiler to remove work through optimization    
+    for (uint32_t i = 0; i < iterations; i++) {
+        generate_random_data(data1, range, n_values);
+        generate_random_data(data2, range, n_values);
+        
+        unified.start();
+        // Call argument subroutine pointer.
+        total += (*f)(data1, data2, n_bitmaps);
+        unified.end(results);
+        allresults.push_back(results);
+    }
+
+    std::vector<unsigned long long> mins = compute_mins(allresults);
+    std::vector<double> avg = compute_averages(allresults);
+    
+    printf("%s-%u:\n",name.c_str(),n_bitmaps);
+    if (verbose) {
+        printf("instructions per cycle %4.2f, cycles per 64-bit word:  %4.3f, "
+               "instructions per 64-bit word %4.3f \n",
+                double(mins[1]) / mins[0], double(mins[0]) / (2*n_bitmaps), double(mins[1]) / (2*n_bitmaps));
+        // first we display mins
+        printf("min: %8llu cycles, %8llu instructions, \t%8llu branch mis., %8llu "
+               "cache ref., %8llu cache mis.\n",
+                mins[0], mins[1], mins[2], mins[3], mins[4]);
+        printf("avg: %8.1f cycles, %8.1f instructions, \t%8.1f branch mis., %8.1f "
+               "cache ref., %8.1f cache mis.\n",
+                avg[0], avg[1], avg[2], avg[3], avg[4]);
+    } else {
+        printf("cycles per 64-bit word:  %4.3f; ref cycles per 64-bit word: %4.3f \n", double(mins[0]) / (2*n_bitmaps), double(mins[5]) / (2*n_bitmaps));
+    }
+
+    return 1;
+}
+
+int linux_popcount_wrapper(std::string name,
+    STORM_popcnt_func f, 
+    int iterations,
+    uint64_t* data,
+    uint32_t range,
+    uint32_t n_values,
+    uint32_t n_bitmaps,
+    bool verbose) 
+{
+    std::vector<int> evts;
+    evts.push_back(PERF_COUNT_HW_CPU_CYCLES);
+    evts.push_back(PERF_COUNT_HW_INSTRUCTIONS);
+    evts.push_back(PERF_COUNT_HW_BRANCH_MISSES);
+    evts.push_back(PERF_COUNT_HW_CACHE_REFERENCES);
+    evts.push_back(PERF_COUNT_HW_CACHE_MISSES);
+    evts.push_back(PERF_COUNT_HW_REF_CPU_CYCLES);
+    LinuxEvents<PERF_TYPE_HARDWARE> unified(evts);
+    std::vector<unsigned long long> results; // tmp buffer
+    std::vector< std::vector<unsigned long long> > allresults;
+    results.resize(evts.size());
+    
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> dis(0, 0xFFFF);
+
+    volatile uint64_t total = 0; // voltatile to prevent compiler to remove work through optimization    
+    for (uint32_t i = 0; i < iterations; i++) {
+        generate_random_data(data, range, n_values);
+        
+        unified.start();
+        // Call argument subroutine pointer.
+        total += (*f)((uint8_t*)data, n_bitmaps*8);
+        unified.end(results);
+        allresults.push_back(results);
+    }
+
+    std::vector<unsigned long long> mins = compute_mins(allresults);
+    std::vector<double> avg = compute_averages(allresults);
+    
+    printf("%s-%u:\n",name.c_str(),n_bitmaps);
+    if (verbose) {
+        printf("instructions per cycle %4.2f, cycles per 64-bit word:  %4.3f, "
+               "instructions per 64-bit word %4.3f \n",
+                double(mins[1]) / mins[0], double(mins[0]) / (2*n_bitmaps), double(mins[1]) / (2*n_bitmaps));
+        // first we display mins
+        printf("min: %8llu cycles, %8llu instructions, \t%8llu branch mis., %8llu "
+               "cache ref., %8llu cache mis.\n",
+                mins[0], mins[1], mins[2], mins[3], mins[4]);
+        printf("avg: %8.1f cycles, %8.1f instructions, \t%8.1f branch mis., %8.1f "
+               "cache ref., %8.1f cache mis.\n",
+                avg[0], avg[1], avg[2], avg[3], avg[4]);
+    } else {
+        printf("cycles per 64-bit word:  %4.3f; ref cycles per 64-bit word: %4.3f \n", double(mins[0]) / (2*n_bitmaps), double(mins[5]) / (2*n_bitmaps));
+    }
+
+    return 1;
+}
+#endif // end is linux
+
 uint8_t popcnt_lookup8bit[256] = {
 	/* 0 */ 0, /* 1 */ 1, /* 2 */ 1, /* 3 */ 2,
 	/* 4 */ 1, /* 5 */ 2, /* 6 */ 2, /* 7 */ 3,
@@ -93,22 +355,6 @@ uint64_t get_cpu_cycles() {
 #endif
     return result;
 };
-
-void generate_random_data(uint64_t* data, uint32_t range, uint32_t n) {
-    // PRNG
-    std::uniform_int_distribution<uint32_t> distr(0, range-1); // right inclusive
-    std::random_device rd;  // obtain a random number from hardware
-    std::mt19937 eng(rd()); // seed the generator
-
-     // Generate some random data.
-    uint32_t n_unique = 0;
-    // while (n_unique < n) {
-    for (int i = 0; i < n; ++i) {
-        uint32_t val = distr(eng);
-        n_unique += (data[val / 64] & (1ULL << (val % 64))) == 0;
-        data[val / 64] |= 1ULL << (val % 64);
-    }
-}
 
 #if !defined(__clang__) && !defined(_MSC_VER)
 __attribute__((optimize("no-tree-vectorize")))
@@ -459,20 +705,31 @@ int benchmark(uint32_t range, uint32_t n_values) {
         uint64_t* bitmaps2 = (uint64_t*)STORM_aligned_malloc(STORM_get_alignment(), 1048576*sizeof(uint64_t));
 
         std::vector<uint32_t> ranges = {4,8,32,128,256,512,1024,2048,4096,8192,65536,262144,1048576};
-        std::vector<uint32_t> iterations = {1000000, 1000000, 1000000, 1000000, 1000000, 50000, 50000, 50000, 50000, 2000, 2000, 2000, 2000};
+        std::vector<uint32_t> iterations = {2000, 2000, 2000, 2000, 2000, 2000, 2000, 2000, 2000, 2000, 2000, 2000, 2000};
         assert(ranges.size() == iterations.size());
 
         for (int i = 0; i < ranges.size(); ++i) {
             bench_unit unit_intsec, unit_union, unit_diff;
+            
+#ifdef __linux__ 
+            linux_popcount_wrapper("popcount-naive",&popcount_scalar_naive_nosimd, iterations[i], bitmaps, range, n_values, ranges[i], true);
+            linux_popcount_wrapper("popcount",&STORM_popcnt, iterations[i], bitmaps, range, n_values, ranges[i], true);
+            linux_set_algebra_wrapper("intersect-naive",&intersect_scalar_naive_nosimd, iterations[i], bitmaps, bitmaps2, range, n_values, ranges[i], true);
+            linux_set_algebra_wrapper("intersect",STORM_get_intersect_count_func(ranges[i]), iterations[i], bitmaps, bitmaps2, range, n_values, ranges[i], true);
+            linux_set_algebra_wrapper("union-naive",&union_scalar_naive_nosimd, iterations[i], bitmaps, bitmaps2, range, n_values, ranges[i], true);
+            linux_set_algebra_wrapper("union",STORM_get_union_count_func(ranges[i]), iterations[i], bitmaps, bitmaps2, range, n_values, ranges[i], true);
+            linux_set_algebra_wrapper("diff-naive",&diff_scalar_naive_nosimd, iterations[i], bitmaps, bitmaps2, range, n_values, ranges[i], true);
+            linux_set_algebra_wrapper("diff",STORM_get_diff_count_func(ranges[i]), iterations[i], bitmaps, bitmaps2, range, n_values, ranges[i], true);
+#else
             popcount_wrapper("popcount-naive",&popcount_scalar_naive_nosimd, iterations[i], bitmaps, range, n_values, ranges[i], unit_intsec);
             popcount_wrapper("popcount",&STORM_popcnt, iterations[i], bitmaps, range, n_values, ranges[i], unit_intsec);
-            
             set_algebra_wrapper("intersect-naive",&intersect_scalar_naive_nosimd, iterations[i], bitmaps, bitmaps2, range, n_values, ranges[i], unit_intsec);
             set_algebra_wrapper("intersect",STORM_get_intersect_count_func(ranges[i]), iterations[i], bitmaps, bitmaps2, range, n_values, ranges[i], unit_intsec);
             set_algebra_wrapper("union-naive",&union_scalar_naive_nosimd, iterations[i], bitmaps, bitmaps2, range, n_values, ranges[i], unit_intsec);
             set_algebra_wrapper("union",STORM_get_union_count_func(ranges[i]), iterations[i], bitmaps, bitmaps2, range, n_values, ranges[i], unit_union);
             set_algebra_wrapper("diff-naive",&diff_scalar_naive_nosimd, iterations[i], bitmaps, bitmaps2, range, n_values, ranges[i], unit_intsec);
-            set_algebra_wrapper("diff",STORM_get_diff_count_func(ranges[i]), iterations[i], bitmaps, bitmaps2, range, n_values, ranges[i], unit_diff);
+            set_algebra_wrapper("diff",STORM_get_diff_count_func(ranges[i]), iterations[i], bitmaps, bitmaps2, range, n_values, ranges[i], unit_diff);      
+#endif
         }
 
         // Clean up.
